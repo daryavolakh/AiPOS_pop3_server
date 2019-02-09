07404635bcbd16fca2c5a35bf6d8bb3740afaa86
AlesiaSk <alesya213@gmail.com>
2019-02-09 08:36:01 +0300

    Added logging, 'fixed' server

---
+++ src/test/java/com/mycompany/aipos_pop3_server/ServerTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aipos_pop3_server;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Алеся
+ */
+public class ServerTest {
+    
+    public ServerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of quit method, of class Server.
+     */
+    @Test
+    public void testQuit() {
+        System.out.println("quit");
+        Server instance = null;
+        instance.quit();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of start method, of class Server.
+     */
+    @Test
+    public void testStart() {
+        System.out.println("start");
+        Server instance = null;
+        instance.start();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Server.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Server.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
