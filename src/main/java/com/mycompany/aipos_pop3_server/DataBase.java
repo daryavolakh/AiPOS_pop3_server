06ee7af3d69982cb7f8abf736c0e99d5f2ce4958
AlesiaSk <alesya213@gmail.com>
2019-03-02 12:31:20 +0300

    Added LIST, RETR, UIDL, NOOP, QUIT, STAT

--- src/main/java/com/mycompany/aipos_pop3_server/DataBase.java
+++ src/main/java/com/mycompany/aipos_pop3_server/DataBase.java
@@ -44,23 +44,123 @@ public class DataBase {
 	}
     }
     
-    public boolean auth(String pass) throws SQLException{
-        Statement statement = connection.createStatement();
-
-			ResultSet resultSet = statement
-					.executeQuery("SELECT password FROM users WHERE password="
-							+ pass + ";");
-			
-        if(resultSet.next()){
-            
-            return true;
-        }
-        else{
-            return false;
-        }
+  
+    
+    public int getNumberOfMessages(String username){
+        int numOfMessage = 0;
+         try { 
+        Statement statement = connection.createStatement(); 
         
+        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
+        while (resultSet.next()) { 
+        numOfMessage++;
+        } 
+        } catch (SQLException e) { 
+        e.getMessage(); 
+        e.printStackTrace(); 
+        } 
+        return numOfMessage; 
     }
     
+    public int getAllCharacters(String username){
+        int numOfChar = 0;
+         try { 
+        Statement statement = connection.createStatement(); 
+        
+        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
+        int i = 0;
+        while (resultSet.next()) { 
+            numOfChar += resultSet.getString("message").length();
+            i++;
+        } 
+        } catch (SQLException e) { 
+        e.getMessage(); 
+        e.printStackTrace(); 
+        } 
+        return numOfChar; 
+    }
+    
+    public boolean checkUser(String user) { 
+        try { 
+        Statement statement = connection.createStatement(); 
+        ResultSet resultSet = statement.executeQuery("SELECT user FROM users WHERE user='" + user + "';"); 
+        while (resultSet.next()) { 
+        return true; 
+        } 
+        } catch (SQLException e) { 
+        e.getMessage(); 
+        e.printStackTrace(); 
+        } 
+        return false; 
+}
+     public String getMessageInfo(String username){
+        String message="";
+        int numOfMes = 0;
+        int numOfChar = 0;
+         try { 
+        Statement statement = connection.createStatement(); 
+        
+        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
+        while (resultSet.next()) { 
+            numOfMes++;
+            message += numOfMes+" "+resultSet.getString("message").length()+"\r\n";
+        } 
+        
+        } catch (SQLException e) { 
+        e.getMessage(); 
+        e.printStackTrace(); 
+        } 
+         message += ".";
+        return message;
+    }
+     
+     public String getMessages(String username){
+        String message="";
+        int numOfMes = 0;
+        int numOfChar = 0;
+         try { 
+        Statement statement = connection.createStatement(); 
+        
+        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
+        while (resultSet.next()) { 
+            numOfMes++;
+            message += numOfMes+" "+resultSet.getString("message")+"\r\n";
+        } 
+        
+        } catch (SQLException e) { 
+        e.getMessage(); 
+        e.printStackTrace(); 
+        } 
+         message += ".";
+        return message;
+    }
+     
+     public String getMessage(String username, int mesInd){
+        String message="";
+        mesInd -=1;
+        int numOfMes = 0;
+        int numOfChar = 0;
+         try { 
+        Statement statement = connection.createStatement(); 
+        
+        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
+        while (resultSet.next()) { 
+            if(mesInd == numOfMes){
+                message += (numOfMes+1)+" "+resultSet.getString("message")+"\r\n";
+            }
+            
+            numOfMes++;
+        } 
+        
+        } catch (SQLException e) { 
+        e.getMessage(); 
+        e.printStackTrace(); 
+        message += "This message not found";
+        } 
+         message += ".";
+        return message;
+    }
+      
     public List<String> getInfo() {
         List<String> list = new ArrayList<String>();
         try {
