27c9ce5a460fc2e64bc9abd9c36fc561c4498afe
Darya <dasha27061999@mail.ru>
2019-03-07 00:48:41 +0300

    Fixed RSET, added DELE

--- src/main/java/com/mycompany/aipos_pop3_server/DataBase.java
+++ src/main/java/com/mycompany/aipos_pop3_server/DataBase.java
@@ -4,44 +4,38 @@ import java.sql.*;
 import com.mysql.fabric.jdbc.FabricMySQLDriver;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.log4j.*;
 import org.apache.log4j.Logger;
 
+//отправка сообщений с аттачментами (архимы, картинки) в разных кодировках
 public class DataBase {
+
     private static final String USERNAME = "root";
     private static final String PASSWORD = "password";
-    private static final String URL = "jdbc:mysql://localhost:3306/aipos?useSSL=false";   
+    private static final String URL = "jdbc:mysql://localhost:3306/aipos?useSSL=false";
     public Logger log = Logger.getLogger(DataBase.class);
 
     private Connection connection;
     private Driver driver;
-    
-    private static final String CHECK_USER = "SELECT user FROM users WHERE user=?;";
-    private static final String CHECK_PASSWORD = "SELECT password FROM users WHERE user=?;";
-    private static final String INSERT_DELETED = "INSERT INTO deletebox VALUES(?,?);";
-    private static final String DELETE_FROM_MAILBOX = "DELETE FROM mailbox WHERE username=? AND message=?;";
-    private static final String INSERT_MAILBOX = "INSERT INTO mailbox VALUES(?,?);";
+
     private static final String DELETE_FROM_DELETEBOX = "DELETE FROM deletebox WHERE username=? AND message=?;";
-    
-    
-    public DataBase(){ 
+    public DataBase() {
         try {
             driver = new FabricMySQLDriver();
-	} catch (SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("Creating driver error!");
             log.info("Creating driver error!");
             return;
-	}
+        }
 
-	try {
+        try {
             DriverManager.registerDriver(driver);
-	} catch (SQLException ex) {
+        } catch (SQLException ex) {
             System.out.println("Can't register driver!");
             log.info("Can't register driver!");
             return;
-	}
+        }
 
-	try {
+        try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("Get connection!");
             log.info("Get connection with DB");
@@ -49,195 +43,118 @@ public class DataBase {
             System.out.println("Can't create connection!");
             log.info("Can't create connection with DB");
             return;
-	}
+        }
     }
-    
-  
-    
-    public int getNumberOfMessages(String username){
+
+    public int getNumberOfMessages(String username) {
         int numOfMessage = 0;
-         try { 
-        Statement statement = connection.createStatement(); 
-        
-        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
-        while (resultSet.next()) { 
-        numOfMessage++;
-        } 
-        } catch (SQLException e) { 
-        e.getMessage(); 
-        e.printStackTrace(); 
-        log.info(e.getMessage());
-        } 
-        return numOfMessage; 
-    }
-    
-    public int getAllCharacters(String username){
-        int numOfChar = 0;
-         try { 
-        Statement statement = connection.createStatement(); 
-        
-        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
-        int i = 0;
-        while (resultSet.next()) { 
-            numOfChar += resultSet.getString("message").length();
-            i++;
-        } 
-        } catch (SQLException e) { 
-        e.getMessage(); 
-        e.printStackTrace();
-        log.info(e.getMessage());
-        } 
-        return numOfChar; 
-    }
-    
-    
-     public String getMessageInfo(String username){
-        String message="";
-        int numOfMes = 0;
-        int numOfChar = 0;
-         try { 
-        Statement statement = connection.createStatement(); 
-        
-        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
-        while (resultSet.next()) { 
-            numOfMes++;
-            message += numOfMes+" "+resultSet.getString("message").length()+"\r\n";
-        } 
-        
-        } catch (SQLException e) { 
-        e.getMessage(); 
-        e.printStackTrace();
-        log.info(e.getMessage());
-        } 
-         message += ".";
-        return message;
-    }
-     
-     public String getMessages(String username){
-        String message="+OK\r\n";
-        int numOfMes = 0;
-        int numOfChar = 0;
-         try { 
-        Statement statement = connection.createStatement(); 
-        
-        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
-        while (resultSet.next()) { 
-            numOfMes++;
-            message += numOfMes+" "+resultSet.getString("message")+"\r\n";
-        } 
-        message += ".";
-        } catch (SQLException e) { 
-        e.getMessage(); 
-        e.printStackTrace(); 
-        message = "-ERR no such message";
-        log.error(e.getMessage());
-        } 
-        
-        return message;
-    }
-     
-     public String getMessage(String username, int mesInd){
-        String message="+OK\r\n";
-        mesInd -=1;
-        int numOfMes = 0;
-        int numOfChar = 0;
-         try { 
-        Statement statement = connection.createStatement(); 
-        
-        ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';"); 
-        while (resultSet.next()) { 
-            if(mesInd == numOfMes){
-                message += (numOfMes+1)+" "+resultSet.getString("message")+"\r\n";
-            }
-            
-            numOfMes++;
-        } 
-         message += ".";
-        } catch (SQLException e) { 
-        e.getMessage(); 
-        e.printStackTrace(); 
-        message = "-ERR no such message";
-        log.error(e.getMessage());
-        } 
-        
-        return message;
-    }
-    
-    public boolean findInTable(String user, String message, String table) {
         try {
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT message FROM " + table + " WHERE user='" + user + "' AND message = '" + message + "';");
+
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';");
             while (resultSet.next()) {
-                return true;
+                numOfMessage++;
             }
         } catch (SQLException e) {
             e.getMessage();
             e.printStackTrace();
-            log.error(e.getMessage());
+            log.info(e.getMessage());
         }
-        return false;
+        return numOfMessage;
     }
 
-    public boolean delete(String user, String message) {
+    public int getAllCharacters(String username) {
+        int numOfChar = 0;
         try {
-            if (findInTable(user, message, "mailbox")) {
-                PreparedStatement stInsert = connection.prepareStatement(INSERT_DELETED);
-                stInsert.setString(1, user);
-                stInsert.setString(2, message);
+            Statement statement = connection.createStatement();
 
-                stInsert.execute();
-
-                PreparedStatement stDelete = connection.prepareStatement(DELETE_FROM_MAILBOX);
-                stDelete.setString(1, user);
-                stDelete.setString(2, message);
-
-                return true;
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';");
+            int i = 0;
+            while (resultSet.next()) {
+                numOfChar += resultSet.getString("message").length();
+                i++;
             }
         } catch (SQLException e) {
             e.getMessage();
             e.printStackTrace();
-            log.error(e.getMessage());
+            log.info(e.getMessage());
         }
-
-        return false;
+        return numOfChar;
     }
-    
-    public void insertFromDeletebox(String user, String message){
+
+    public String getMessageInfo(String username) {
+        String message = "";
+        int numOfMes = 0;
+        int numOfChar = 0;
         try {
-            if (findInTable(user, message, "deletebox")) {
-                PreparedStatement stInsert = connection.prepareStatement(INSERT_MAILBOX);
-                stInsert.setString(1, user);
-                stInsert.setString(2, message);
+            Statement statement = connection.createStatement();
 
-                stInsert.execute();
-
-                PreparedStatement stDelete = connection.prepareStatement(DELETE_FROM_DELETEBOX);
-                stDelete.setString(1, user);
-                stDelete.setString(2, message);
-
-                stDelete.execute();
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';");
+            while (resultSet.next()) {
+                numOfMes++;
+                message += numOfMes + " " + resultSet.getString("message").length() + "\r\n";
             }
+
         } catch (SQLException e) {
             e.getMessage();
             e.printStackTrace();
-            log.error(e.getMessage());
+            log.info(e.getMessage());
         }
+        message += ".";
+        return message;
     }
-    
-    public void deleteAllFromDeletebox(String user){
+
+    public String getMessages(String username) {
+        String message = "+OK\r\n";
+        int numOfMes = 0;
+        int numOfChar = 0;
         try {
-            PreparedStatement stDelete = connection.prepareStatement("DELETE FROM deletebox WHERE username='" + user + "';");
-            stDelete.setString(1, user);
-            
-            stDelete.execute();
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';");
+            while (resultSet.next()) {
+                numOfMes++;
+                message += numOfMes + " " + resultSet.getString("message") + "\r\n";
+            }
+            message += ".";
         } catch (SQLException e) {
             e.getMessage();
             e.printStackTrace();
+            message = "-ERR no such message";
             log.error(e.getMessage());
         }
+
+        return message;
     }
-    
-      public boolean checkUser(String user) {
+
+    public String getMessage(String username, int mesInd) {
+        String message = "+OK\r\n";
+        mesInd -= 1;
+        int numOfMes = 0;
+        int numOfChar = 0;
+        try {
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + username + "';");
+            while (resultSet.next()) {
+                if (mesInd == numOfMes) {
+                    message += (numOfMes + 1) + " " + resultSet.getString("message") + "\r\n";
+                }
+
+                numOfMes++;
+            }
+            message += ".";
+        } catch (SQLException e) {
+            e.getMessage();
+            e.printStackTrace();
+            message = "-ERR no such message";
+            log.error(e.getMessage());
+        }
+        
+        return message;
+    }
+
+    public boolean checkUser(String user) {
         try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(("SELECT username FROM users WHERE username='" + user + "';"));
@@ -270,5 +187,77 @@ public class DataBase {
         }
 
         return false;
+    }
+
+    public boolean findInTable(String user, String message, String table) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM " + table + " WHERE username='" + user + "' AND message = '" + message + "';");
+            while (resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean delete(String user, int number) {
+        List<String> list = new ArrayList<String>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT message FROM mailbox WHERE username='" + user + "'");
+
+            while (resultSet.next()) {
+                list.add(resultSet.getString(1));
+            }
+            
+            String message = list.get(number - 1);
+
+            if (findInTable(user, message, "mailbox")) {
+                PreparedStatement stUpdate = connection.prepareStatement("UPDATE mailbox SET deleted=true WHERE username='" + user + "' AND message='" + message + "' AND deleted=false;");
+                            
+                stUpdate.execute();
+
+                return true;
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+
+        return false;
+    }
+
+    public void insertFromDeletebox(String user, String message) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE mailbox SET deleted=false WHERE username='" + user + "' AND message='" + message + "' AND deleted=true;");
+            
+            ResultSet resultSet = statement.executeQuery();      
+
+            while (resultSet.next()) {
+                statement.execute();
+            }
+           
+        } catch (SQLException e) {
+            e.getMessage();
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+    }
+
+    public void deleteAllFromDeletebox(String user) {
+        try {
+            PreparedStatement stDelete = connection.prepareStatement("DELETE FROM mailbox WHERE username='" + user + "'AND deleted=true;");
+
+            stDelete.execute();
+        } catch (SQLException e) {
+            e.getMessage();
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
     }
 }
--- src/main/java/com/mycompany/aipos_pop3_server/ServerHandler.java
+++ src/main/java/com/mycompany/aipos_pop3_server/ServerHandler.java
@@ -4,9 +4,6 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import org.apache.log4j.Logger;
-import java.sql.SQLException;
-import java.util.Base64.Decoder;
-import java.util.logging.Level;
 
 /*This class process data recieved from client  through one socket connection*/
 class ServerHandler implements Runnable {
@@ -15,14 +12,14 @@ class ServerHandler implements Runnable {
     public Server server;
     public DataBase db;
     public Logger log = Logger.getLogger(ServerHandler.class);
-    
+
     public ServerHandler(Socket incoming, Server incomingserver) {
         this.incoming = incoming;
         server = incomingserver;
         db = new DataBase();
     }
 
-    public void run() {        
+    public void run() {
         log.info("New client");
         String username = "";
         String info = "";
@@ -30,27 +27,27 @@ class ServerHandler implements Runnable {
         try (InputStream inStream = incoming.getInputStream();
                 OutputStream outStream = incoming.getOutputStream()) {
             Scanner in = new Scanner(inStream, "UTF-8");
-            
+
             PrintWriter out = new PrintWriter(
                     new OutputStreamWriter(outStream, "UTF-8"), true);
+            /* true - auto cleanning*/
+
             String message = "+OK POP3 server ready <>";
             out.println(message);
             log.info("S: '" + message);
             //send back client's data
             boolean done = false;
             while (!done && in.hasNextLine()) {
-                String line = in.nextLine(); 
-                if (line.contains(" ")){ 
-                    int index = line.indexOf(" "); 
-                    info = line.substring(index + 1, line.length()); 
-                    command = line.substring(0, index); 
-                    } 
-
-                    else { 
-                    command = line; 
-                    }
+                String line = in.nextLine();
+                if (line.contains(" ")) {
+                    int index = line.indexOf(" ");
+                    info = line.substring(index + 1, line.length());
+                    command = line.substring(0, index);
+                } else {
+                    command = line;
+                }
                 log.info("C: '" + line);
-               if (command.trim().equals("QUIT")) {
+                if (command.trim().equals("QUIT")) {
                     db.deleteAllFromDeletebox(username);
                     message = "+OK dewey POP3 server signing off";
                     out.println(message);
@@ -58,9 +55,9 @@ class ServerHandler implements Runnable {
                     done = true;
                     break;
                 }
-               
-               else if(command.trim().equals("USER")){
-                   username = info;
+
+                if (command.trim().equals("USER")) {
+                    username = info;
 
                     if (db.checkUser(info)) {
                         message = "+OK User accepted";
@@ -71,10 +68,8 @@ class ServerHandler implements Runnable {
                         out.println(message);
                         log.error("S: '" + message);
                     }
-               }
-               
-               else if(command.trim().equals("PASS")){
-                   if (db.checkPass(username, info)) {
+                } else if (command.trim().equals("PASS")) {
+                    if (db.checkPass(username, info)) {
                         message = "+OK Pass accepted";
                         out.println(message);
                         log.info("S: '" + message);
@@ -83,17 +78,13 @@ class ServerHandler implements Runnable {
                         out.println(message);
                         log.error("S: '" + message);
                     }
-               }
-               
-               else if (command.trim().equals("RSET")) {
+                } else if (command.trim().equals("RSET")) {
                     db.insertFromDeletebox(username, info);
                     message = "+OK";
                     out.println(message);
                     log.info("S: '" + message);
-                } 
-               
-               else if (command.trim().equals("DELE")) {
-                    if (db.delete(username, info)) {
+                } else if (command.trim().equals("DELE")) {
+                    if (db.delete(username, Integer.parseInt(info))) {
                         message = "+OK message deleted";
                         out.println(message);
                         log.info("S: '" + message);
@@ -102,65 +93,45 @@ class ServerHandler implements Runnable {
                         out.println(message);
                         log.error("S: '" + message);
                     }
-                } 
-                
-                else if (command.trim().equals("TOP")) {
+                } else if (command.trim().equals("TOP")) {
                     // +OK top of message follows
                     //-ERR no such message
                     message = "+OK top of message follows";
                     out.println(message);
                     log.info("S: '" + message);
-                } 
-                
-                else if (command.trim().equals("APOP")) {
+                } else if (command.trim().equals("APOP")) {
                     //+OK maildrop locked and ready
                     //-ERR permission denied
                     message = "+OK maildrop locked and ready";
                     out.println(message);
                     log.info("S: '" + message);
-                }
-               
-               else if(command.trim().equals("STAT")){
-                   if(db.getNumberOfMessages(username) > 0){
-                       out.println("+OK "+ db.getNumberOfMessages(username)+" "+ db.getAllCharacters(username));
-                       log.info("S: +OK "+ db.getNumberOfMessages(username)+" "+ db.getAllCharacters(username));
-                   }
-                   else{
-                       out.println("-ERR no such message");
-                       log.error("-ERR no such message");
-                   }
-                   
-               }
-               
-               else if(command.trim().equals("LIST")){
-                   if(db.getNumberOfMessages(username) > 0){
-                       out.println("+OK"+ db.getNumberOfMessages(username)+"  messages ("+db.getAllCharacters(username)+" octets)\r\n");
-                       out.println(db.getMessageInfo(username));
-                       log.info("+OK "+ db.getNumberOfMessages(username)+"  messages ("+db.getAllCharacters(username)+" octets)"+ db.getMessageInfo(username));
-                   }
-                   else{
-                       out.println("-ERR no such message");
-                       log.error("-ERR no such message");
-                   }
-                   
-               }
-               
-               else if(command.trim().equals("UIDL")){
-                   out.println(db.getMessages(username));
-                   log.info("S: " + db.getMessages(username));
-               }
-               
-               else if(command.equals("RETR")){
-                   out.println(db.getMessage(username, Integer.parseInt(info)));
-                   log.info("S: " + db.getMessage(username, Integer.parseInt(info)));
-               }
-               
-               else if (command.trim().equals("NOOP")) { 
-                out.println("+OK");
-                log.info("S: +OK");
-                }
-               
-               else {
+                } else if (line.trim().equals("STAT")) {
+                    if (db.getNumberOfMessages(username) > 0) {
+                        out.println("+OK " + db.getNumberOfMessages(username) + " " + db.getAllCharacters(username));
+                        log.info("S: +OK " + db.getNumberOfMessages(username) + " " + db.getAllCharacters(username));
+                    } else {
+                        out.println("-ERR no such message");
+                        log.error("-ERR no such message");
+                    }
+                } else if (line.trim().equals("LIST")) {
+                    if (db.getNumberOfMessages(username) > 0) {
+                        out.println("+OK" + db.getNumberOfMessages(username) + "  messages (" + db.getAllCharacters(username) + " octets)\r\n");
+                        out.println(db.getMessageInfo(username));
+                        log.info("+OK " + db.getNumberOfMessages(username) + "  messages (" + db.getAllCharacters(username) + " octets)" + db.getMessageInfo(username));
+                    } else {
+                        out.println("-ERR no such message");
+                        log.error("-ERR no such message");
+                    }
+                } else if (line.trim().equals("UIDL")) {
+                    out.println(db.getMessages(username));
+                    log.info("S: " + db.getMessages(username));
+                } else if (command.equals("RETR")) {
+                    out.println(db.getMessage(username, Integer.parseInt(info)));
+                    log.info("S: " + db.getMessage(username, Integer.parseInt(info)));
+                } else if (line.trim().equals("NOOP")) {
+                    out.println("+OK");
+                    log.info("S: +OK");
+                } else {
                     message = "-ERR invalid command " + line;
                     out.println(message);
                     log.error("S: '" + message);
@@ -168,7 +139,7 @@ class ServerHandler implements Runnable {
             }
         } catch (IOException exception) {
             System.out.println("Can't get input data");
-            log.error("Can't get inout data");
-        } 
+            log.error("Can't get input data");
+        }
     }
 }
